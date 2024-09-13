767a21bcd999f81b207c80c1b2f2bb944a678801
muhammadnabeelawan <142487216+muhammadnabeelawan@users.noreply.github.com>
2024-09-13 19:10:19 +0500

    deploy

--- database/migrations/2024_09_11_162037_add_username_to_users_table.php
+++ database/migrations/2024_09_11_162037_add_username_to_users_table.php
@@ -14,7 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('username')->unique(); 
+          
+            
+            $table->string('username'); 
+          
+            
         });
     }
 
@@ -27,6 +31,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('username');
+           
         });
     }
 };
